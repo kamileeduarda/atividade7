b17f72bce3ed18ea628fdc6cf8e5d3e499c81e7a
kamileeduarda <91344607+kamileeduarda@users.noreply.github.com>
2021-10-23 13:27:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let nome = prompt('Digite o seu nome');
+let ano = prompt('Digite o seu ano de nascimento');
+let idade = 2021 - ano;
+seu_nome = `Olá, seu nome é ${nome} e você tem ${idade} anos.`
+alert(seu_nome)
